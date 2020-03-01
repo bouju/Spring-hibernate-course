9d3a717b99c4c0ef279fc57c7a75f559981127a2
Mathis BOUJU <mbouju@squad.fr>
2020-03-01 17:23:56 +0100

    feat(XML configuration): inversion of control

---
+++ My-Work/spring-demo-one/src/com/springdemo/TrackmaniaCoach.java
@@ -0,0 +1,11 @@
+package com.springdemo;
+
+public class TrackmaniaCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+
+		return "Practice Rammstein into B2 expand !";
+	}
+
+}
